b249396caa595709853b3668719c8d65bb521e37
Deep <diptiprasadsarangi44@gmail.com>
2025-07-02 01:05:07 +0530

    update font path

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/retroriotes-games",
+  base: "/retroriotes-games/",
   plugins: [react(), tailwindcss()],
 });
